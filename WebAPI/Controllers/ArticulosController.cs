e3461943db85f3c6f838535518dbc3cd2729f669
FalfanJ <falfan.jos@hotmail.com>
2025-10-15 06:10:06 -0300

    Agregar

--- WebAPI/Controllers/ArticulosController.cs
+++ WebAPI/Controllers/ArticulosController.cs
@@ -1,11 +1,13 @@
 ﻿using Dominio;
 using Negocio;
+using WebAPI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.UI.WebControls.WebParts;
 
 namespace WebAPI.Controllers
 {
@@ -24,8 +26,20 @@ namespace WebAPI.Controllers
         }
 
         // POST: api/Articulos
-        public void Post([FromBody]string value)
+        public void Post([FromBody]ArticulosDto art)
         {
+            ArticulosNegocio artNeg = new ArticulosNegocio();
+            Articulos nuevo = new Articulos();
+            nuevo.Marca = new Marcas();
+            nuevo.Categoria = new Categorias();
+
+            nuevo.Codigo = art.Codigo;
+            nuevo.Nombre = art.Nombre;
+            nuevo.Descripcion = art.Descripcion;
+            nuevo.Precio = art.Precio;
+            nuevo.Marca.Id = art.IdMarca;
+            nuevo.Categoria.Id = art.IdCategoria;
+            artNeg.Agregar(nuevo);
         }
 
         // PUT: api/Articulos/5
